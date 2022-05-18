155c5a83f75a5dd2e3d82ae993d1ae2ab2a04ef6
Idiat Ameen <idi@Idiats-Air.lan>
2022-05-17 21:43:19 -0400

    container

---
+++ containerwater.js
@@ -0,0 +1,21 @@
+// function getMaxWater(heights) {
+//   for (a = 0; a < heights.length; a++){
+//     let maxArea = 0
+//     for(b)
+//   }
+// }
+
+function getMaxWaterContainer(heights) {
+  let maxArea = 0;
+  for (let p1 = 0; p1 < heights.length; p1++) {
+    for (let p2 = p1 + 1; p2 < heights.length; p2++) {
+      const height = Math.min(heights[p1], heights[p2]);
+      const width = p2 - p1;
+      const area = height * width;
+      maxArea = Math.max(maxArea, area);
+    }
+  }
+  return maxArea;
+}
+
+console.log(getMaxWaterContainer([7, 1, 2, 3, 9]));
